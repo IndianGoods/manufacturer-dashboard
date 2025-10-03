e0f826bfec3a5ac37e138514fb3b86fc12e39953
Shashwat Rai <shashwatrai575@gmail.com>
2025-10-03 17:01:52 +0530

    Order Detail Page

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ import Home from './pages/dashboard/Home'
 import Products from './pages/dashboard/Products'
 import Inventory from './pages/dashboard/Inventory'
 import ProductDetail from './pages/dashboard/ProductDetail'
+import Orders from './pages/dashboard/orders/Orders'
+import OrderDetailPage from './pages/dashboard/orders/OrderDetailPage'
 
 function App() {
   return (
@@ -33,12 +35,14 @@ function App() {
             <Route path="products/:id" element={<ProductDetail />} />
             <Route path="inventory" element={<Inventory />} />
             <Route path="products/inventory" element={<Inventory />} />
-            <Route path="orders" element={<div>Orders Page</div>} />
+            <Route path="orders" element={<Orders />} />
+            <Route path="orders/:id" element={<OrderDetailPage />} />
             <Route path="rfqs" element={<div>RFQs Page</div>} />
             <Route path="discounts" element={<div>Discounts Page</div>} />
             <Route path="analytics" element={<div>Analytics Page</div>} />
             <Route path="support" element={<div>Support Page</div>} />
             <Route path="settings" element={<div>Settings Page</div>} />
+
           </Route>
           
           {/* Redirect root to dashboard */}
